84bde2a16535935d4af8e38b51376148812624f3
Vicente Castillo M <91623084+Vichh0@users.noreply.github.com>
2025-06-20 11:20:39 -0400

    ?

--- lib/pages/camera.dart
+++ lib/pages/camera.dart
@@ -11,7 +11,20 @@ class PictureScreen extends StatefulWidget {
   _PictureScreenState createState() => _PictureScreenState();
 
 }
-
+/*
+class PreviewPictureScreen extends StatelessWidget {
+  final String imagePath;
+  const PreviewPictureScreen({super.key, required this.imagePath});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Vista previa')),
+      body: Image.file(File(imagePath)),
+      );
+  }
+}
+*/
 class _PictureScreenState extends State<PictureScreen> {
 
   late CameraController _controller;
@@ -39,8 +52,52 @@ class _PictureScreenState extends State<PictureScreen> {
       appBar: AppBar(
         title: Text('Picture Screen'),
       ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Menú',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              onTap: () {
+                Navigator.of(context).pop(); // Close the drawer
+                Navigator.of(context).pushNamed('/'); // Navigate to home
+              },
+            ),
+          ],
+        ),
+      ),
       body: Center(
-        child: Text('Camera: ${widget.camera.name}'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Camera: ${widget.camera.name}'),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                try {
+                  await _initializeControllerFuture;
+                  final image = await _controller.takePicture();
+                } catch (e) {
+                  print(e);
+                  }
+              },
+              child: Text('Tomar foto'),
+            ),
+          ],
+        ),
       ),
     );
   }
